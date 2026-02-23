6a82df1a1ee0c2902a057b7831e99af24e93c5aa
fightsurf <100633062+fightsurf@users.noreply.github.com>
2026-02-23 05:09:17 -0300

    Remove redundant comments and update module export

--- src/routes/logisticaRoutes.js
+++ src/routes/logisticaRoutes.js
@@ -2,16 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/logisticaController');
 
-// Vincular cidade à transportadora
 router.post('/transportadoras/:id/cidades', controller.vincular);
-
-// Listar cidades da transportadora
+router.delete('/transportadoras/:id/cidades/:codigo_ibge', controller.remover);
 router.get('/transportadoras/:id/cidades', controller.cidades);
-
-// Listar transportadoras por cidade
 router.get('/cidades/:codigo_ibge/transportadoras', controller.transportadoras);
-
-// Buscar cidades por nome (autocomplete frontend)
 router.get('/cidades/busca', controller.buscarCidades);
 
 module.exports = router;
