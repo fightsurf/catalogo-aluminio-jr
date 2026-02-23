392c1841be2269731be14aceb63fdeab231d5d14
fightsurf <100633062+fightsurf@users.noreply.github.com>
2026-02-23 11:42:58 -0300

    Update transportadoraService.js

--- src/services/transportadoraService.js
+++ src/services/transportadoraService.js
@@ -8,33 +8,34 @@ async function listarTransportadoras() {
 }
 
 async function criarTransportadora(dados) {
-  const { nome, contato_principal, telefone, observacoes } = dados;
+  const { nome, telefone, observacao } = dados;
 
   const result = await pool.query(
     `
     INSERT INTO transportadoras
-    (nome, contato_principal, telefone, observacoes)
-    VALUES ($1, $2, $3, $4)
+    (nome, telefone, observacao)
+    VALUES ($1, $2, $3)
     RETURNING *;
     `,
-    [nome, contato_principal || null, telefone || null, observacoes || null]
+    [nome, telefone || null, observacao || null]
   );
 
   return result.rows[0];
 }
 
 async function atualizarTransportadora(id, dados) {
-  const { nome, telefone } = dados;
+  const { nome, telefone, observacao } = dados;
 
   const result = await pool.query(
     `
     UPDATE transportadoras
     SET nome = $1,
-        telefone = $2
-    WHERE id = $3
+        telefone = $2,
+        observacao = $3
+    WHERE id = $4
     RETURNING *;
     `,
-    [nome, telefone, id]
+    [nome, telefone, observacao, id]
   );
 
   return result.rows[0];
