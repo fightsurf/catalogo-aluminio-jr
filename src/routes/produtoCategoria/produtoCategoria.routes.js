53b9f94317630c15d620c36cfb559e4642f462f8
fightsurf <100633062+fightsurf@users.noreply.github.com>
2026-02-27 22:54:40 -0300

    Update produtoCategoria.routes.js

--- src/routes/produtoCategoria/produtoCategoria.routes.js
+++ src/routes/produtoCategoria/produtoCategoria.routes.js
@@ -1,1 +1,12 @@
 
+const express = require('express');
+const router = express.Router();
+const controller = require('../../controllers/produtoCategoria/produtoCategoria.controller');
+
+router.get('/', controller.listar);
+router.get('/:id', controller.buscar);
+router.post('/', controller.criar);
+router.put('/:id', controller.atualizar);
+router.delete('/:id', controller.excluir);
+
+module.exports = router;
