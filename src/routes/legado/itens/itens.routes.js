6dd3212ac0d5ed38e8836d0e80b6928667540ed7
fightsurf <100633062+fightsurf@users.noreply.github.com>
2026-03-25 20:16:19 -0300

    Add files via upload

--- src/routes/legado/itens/itens.routes.js
+++ src/routes/legado/itens/itens.routes.js
@@ -7,5 +7,8 @@ router.get('/', itensController.listarItens);
 router.get('/:item', itensController.buscarItem);
 router.put('/:item/descricao', itensController.atualizarDescricao);
 router.post('/sincronizar-descricao', itensController.sincronizarDescricaoProduto);
+router.post('/associar', itensController.associarProdutoAoItemLegado);
+router.post('/transferir-associacao', itensController.transferirAssociacaoProdutoAoItemLegado);
+router.delete('/associacao/:produtoId', itensController.desassociarProdutoDoItemLegado);
 
 module.exports = router;
